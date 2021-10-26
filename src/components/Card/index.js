ef419c181b3161a783becafce4f78ffdb0bfca96
harshitasrivasatva <harshitasriavastava376@gmail.com>
2021-10-26 21:40:43 +0530

    creating card with css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
+import "../../assets/css/card.css"
+
 function index({ title, image, body }) {
     return (
-        <div className="card mb-5" style={{ width:"350px" , boxShadow: "0 16px 8px 3px lightgrey", borderRadius: "12px"}}>
-            <img className="card-img-top" src={image} alt="Card image" style={{width:"100%", height:"200px"}} />
-            <div className="card-body">
-              <h4 className="card-title text-primary px-3 font-weight-bold">{title}</h4>
-              <p className="card-text text-secondary p-3">{body}</p>
-            </div>
-      </div>
+        <div className="card">
+            <img src={image}/>
+            <h3>{title}</h3> 
+            <p> {body} </p>
+            <div className="card-overlay"></div>
+        </div>
     )
 }
 
